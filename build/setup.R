cd2e854c693bf23892f926afcbc9772b11cbf807
yahiaBioinfo <yahia.adnani@gustaveroussy.fr>
2019-03-12 14:34:31 +0100

    BUILD docker

---
+++ build/setup.R
@@ -0,0 +1,27 @@
+if(!require(stringr))
+  install.packages("stringr",repos='https://cloud.r-project.org/')
+
+if(!require(foreach))
+  install.packages("foreach",repos='https://cloud.r-project.org/')
+
+if(!require(shinycssloaders))
+  install.packages("shinycssloaders",repos='https://cloud.r-project.org/')
+
+
+if(!require(shinydashboard))
+  install.packages("shinydashboard",repos='https://cloud.r-project.org/')
+
+if(!require(plotly))
+  install.packages("plotly",repos='https://cloud.r-project.org/')
+
+if(!require(shiny))
+  install.packages("shiny",repos='https://cloud.r-project.org/')
+
+if(!require(shinyjs))
+  install.packages("shinyjs",repos='https://cloud.r-project.org/')
+
+if(!require(DT))
+  install.packages("DT",repos='https://cloud.r-project.org/')
+
+if(!require(jsonlite))
+  install.packages("jsonlite",repos='https://cloud.r-project.org/')
